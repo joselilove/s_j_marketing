7d0c4f8d26f3304767d8a14a226ece99066c0246
root <root@ip-172-0-1-61.ap-southeast-1.compute.internal>
2021-01-17 08:15:36 +0000

    SJ-1 fixed version

--- template/plugins.php
+++ template/plugins.php
@@ -39,7 +39,7 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 
     <script src="js/bootstrap.js"></script>
     <script src="js/jquery.dcjqaccordion.2.7.js"></script>
-    <script src="js/scripts?v=12.js"></script>
+    <script src="js/scripts.js"></script>
     <script src="js/jquery.slimscroll.js"></script>
     <script src="js/jquery.nicescroll.js"></script>
     <!--[if lte IE 8]><script language="javascript" type="text/javascript" src="js/flot-chart/excanvas.min.js"></script><![endif]-->
